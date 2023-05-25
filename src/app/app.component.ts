e543a8f193cb27d3a79d93aed0bfafcd301ca64d
Ayushi Gupta <Ayushi.Gupta@geminisolutions.com>
2023-05-25 14:21:28 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+ 
+ data="";
+ value='true';
+  showData(type:string){
+    
+    if(this.value=='true'){
+     this.data="Ayushi";
+     this.value='false';
+    }else{
+
+      this.data="";
+      this.value='true';
+    }
+
+  }
+
+  content="";
+  countWords=1;
+  addInput(content:string){
+    let para=document.getElementById('para');
+   
+   
+    // this.content=content;
+   
+    if(this.countWords%5==0){
+      let span=document.createElement('span');
+      span.innerHTML=" "+content;
+      span.style.color="blue";
+      para?.appendChild(span);
+      this.countWords++;
+    }else{
+    
+      let span=document.createElement('span');
+      span.innerHTML=" "+content;
+      para?.appendChild(span);
+      this.countWords++;
+
+    }
+
+
+  }
+
+
 }
